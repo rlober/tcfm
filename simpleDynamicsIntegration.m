87f3d4e831212ede7c94566c3e581048a1f92408
Ryan <rlober@gmail.com>
2017-05-26 14:28:37 +0200

    Use globals

--- simpleDynamicsIntegration.m
+++ simpleDynamicsIntegration.m
@@ -1,13 +1,14 @@
-function [ t, y ] = simpleDynamicsIntegration( robot, controller, use_friction, tspan, y0 )
+function [ t, y ] = simpleDynamicsIntegration(  use_friction, tspan, y0 )
 %SIMPLEDYNAMICSINTEGRATION Summary of this function goes here
 %   Detailed explanation goes here
-
+global robot;
+global controller;
 dt = tspan(1)-tspan(2);
 y = ones(size(tspan,2),size(y0,1));
 y(1,:) = y0';
 for i = 1:size(tspan,2)-1
     t = tspan(i);
-    dydt = dynamics(t, y(i,:)', robot, controller, use_friction);
+    dydt = dynamics(t, y(i,:)', use_friction);
     y(i+1,:) = y(i,:)' + dydt * dt;
 end
 
